2dc9044a1a4334c110effc8d028ec54071106d7e
ztt <495359406@qq.com>
2018-03-10 17:12:27 +0800

    123

---
+++ 123/123/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  123
+//
+//  Created by jz100ios on 2018/3/10.
+//  Copyright © 2018年 ZT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 123/123/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  123
+//
+//  Created by jz100ios on 2018/3/10.
+//  Copyright © 2018年 ZT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
